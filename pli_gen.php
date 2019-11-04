4bbf03e9158860bdcc36432ea5470632758455c0
Mike Adamson <madamson@datastax.com>
2019-11-04 10:52:43 +0000

    Latest version

--- pli_gen.php
+++ pli_gen.php
@@ -11,22 +11,25 @@ register_uninstall_hook( __FILE__, 'pli_uninstall' );
 add_action('init', 'pli_initialise_shortcode');
 
 if ( is_admin() ) {
-    add_action( 'admin_init', 'pli_register_settings' );
     add_action( 'admin_menu', 'pli_register_settings_page');
 }
 
-global $pli_option;
-define( 'PLI_OPTION_NAME', 'pli_gen' );
-$pli_option = ABSPATH . get_option( PLI_OPTION_NAME );
-if (file_exists($pli_option)) {
-    pli_log("pli_option = ".$pli_option." exists");
-} else {
-    pli_log("pli_option = ".$pli_option." does not exist");
-}
+define( 'PLI_OPTION_NAME', 'pligen' );
 
 function pli_activate() {
     pli_log("pli_activate called");
-    mkdir(ABSPATH."/wp-content/pli_gen", 0644);
+    $cache_folder = ABSPATH."/wp-content/pli_gen";
+    if (!file_exists($cache_folder)) {
+        mkdir($cache_folder, 0644);
+    }
+    $options = get_option(PLI_OPTION_NAME);
+    if ( !$options ) $options = array();
+    $update_options = array(
+        'template' => array_key_exists('template', $options) ? $options['template'] : '',
+        'offset' => array_key_exists('offset', $options) ? $options['offset'] : '350',
+        'fontsize' => array_key_exists('fontsize', $options) ? $options['fontsize'] : '17'
+    );
+    update_option(PLI_OPTION_NAME, $update_options );
 }
 
 function pli_initialise_shortcode() {
@@ -35,26 +38,66 @@ function pli_initialise_shortcode() {
 }
 
 function pli_shortcode_handler() {
-    global $pli_option;
-    pli_log("pli_shortcode_handler called ");
+    pli_log("pli_shortcode_handler called");
+    $options = get_option(PLI_OPTION_NAME);
+    $template = $options['template'];
+    $offset = $options['offset'];
+    $fontsize = $options['fontsize'];
+
+    if (empty($template)) {
+        return '<p>PLI certificates are not currently available</p>';
+    }
     $current_user = wp_get_current_user();
     $team_name = $current_user->display_name;
-    $words = explode(' ', $team_name);
 
-    $line = "Test Team";
+    pli_log("Team name = ".$team_name);
+
+    $words = explode(' ', $team_name);
+    $line = NULL;
+    $returns = 1;
+    foreach ($words as $word) {
+        $temp = NULL;
+        if (empty($line)) {
+            $temp = $word;
+        } else {
+            $temp = $line.' '.$word;
+        }
+        if (count($temp) > (40 * $returns)) {
+            $temp = $line."\n".$word;
+            $returns++;
+        }
+        $line = $temp;
+    }
+    $line = str_replace('"', '\"', $line);
+    pli_log("Folded team name = ".$line);
 
     $pdf_file_name = 'pli_'.$current_user->user_login.'.pdf';
     $thumbnail_file_name = $pdf_file_name.'.jpg';
     $pdf_file_path = ABSPATH.'wp-content/pli_gen/'.$pdf_file_name;
+    pli_log($pdf_file_path);
     $thumbnail_file_path = $pdf_file_path . '.jpg';
+    $name_file_path = $pdf_file_path.'name.txt';
 
-    if (!file_exists($pdf_file_path)) {
+    $name_matches = false;
+
+    if (file_exists($name_file_path)) {
+        $current_team_name = file_get_contents($name_file_path);
+        if (strcmp($team_name, $current_team_name) == 0) {
+            $name_matches = true;
+        }
+    }
+    if (!$name_matches) {
+        file_put_contents($name_file_path, $team_name);
+    }
+
+    if (!$name_matches || !file_exists($pdf_file_path)) {
         $cpdf = ABSPATH.'scripts/cpdf';
-        $command=$cpdf.' -add-text "'.$line.'" -top 350 -justify-center -midline -font "Helvetica" -font-size 17 '.$pli_option.' -o '.$pdf_file_path_path;
+        $command=$cpdf.' -add-text "'.$line.'" -top '.$offset.' -justify-center -midline -font "Helvetica" -font-size '.$fontsize.' '.ABSPATH.$template.' -o '.$pdf_file_path;
+        pli_log($command);
         shell_exec($command);
     }
 
-    if (!file_exists($thumbnail_file_path)) {
+    if (!$name_matches || !file_exists($thumbnail_file_path)) {
         $pdfThumb = new imagick();
         $pdfThumb->setResolution(32, 32);
         $pdfThumb->readImage($pdf_file_path . '[0]');
@@ -67,35 +110,46 @@ function pli_shortcode_handler() {
     return '<a href="'.$url.'/wp-content/pli_gen/'.$pdf_file_name.'"><img class="alignnone size-medium thumb-of-pdf" src="'.$url.'/wp-content/pli_gen/'.$thumbnail_file_name.'" alt="thumbnail of pli" width="212" height="300" /></a>';
 }
 
-function pli_register_settings() {
-    pli_log("pli_register_settings called");
-
-    add_option(PLI_OPTION_NAME, '');
-
-    register_setting(PLI_OPTION_NAME, PLI_OPTION_NAME);
-}
-
 function pli_register_settings_page() {
-    add_options_page("PLI Certificate Generation Settings", "PLI Certificate Generation", "manage_options", PLI_OPTION_NAME, 'pli_options_page');
+    add_options_page("PLI Certificate Generation Settings", "PLI Certificate Generation", "manage_options", __FILE__, 'pli_options_page');
 }
 
 function pli_options_page() {
     pli_log('pli_options_page called');
-?>
-  <div>
-  <h2>PLI Certificate Generation</h2>
-  <form method="post" action="options.php">
-  <?php settings_fields(PLI_OPTION_NAME); ?>
-  <table>
-  <tr valign="top">
-  <th scope="row"><label for="pli_gen">PLI Certificate Template Path</label></th>
-  <td><input type="text" id="pli_gen" name="pli_gen" value="<?php echo get_option('pli_gen'); ?>" /></td>
-  </tr>
-  </table>
-  <?php  submit_button(); ?>
-  </form>
-  </div>
-<?php
+    if ( isset( $_POST[ 'pli_gen_options_nonce' ] ) && wp_verify_nonce( $_POST[ 'pli_gen_options_nonce' ], basename( __FILE__ ) ) ) {
+        $update_options = array(
+            'template' => isset($_POST['pli_gen_template']) ? $_POST[ 'pli_gen_template' ] : '',
+            'offset' => isset($_POST['offset']) ? $_POST['offset'] : '350',
+            'fontsize' => isset($_POST['fontsize']) ? $_POST['fontsize'] : '17'
+        );
+        update_option(PLI_OPTION_NAME, $update_options );
+        echo '<div class="updated fade"><p><strong>Options saved</strong></p></div>';
+    }
+
+    $options = get_option(PLI_OPTION_NAME);
+
+    echo
+        '<div>'."\n".
+        '<h2>PLI Certificate Generation</h2>'."\n".
+        '<form method="post" action="">'."\n".
+        '<table>'."\n".
+        '<tr valign="top">'."\n".
+        '<th scope="row"><label for="pli_gen_template">Template Path</label></th>'."\n".
+        '<td><input type="text" id="pli_gen_template" name="pli_gen_template" value="'.($options['template']).'"/></td>'."\n".
+        '</tr>'."\n".
+        '<tr valign="top">'."\n".
+        '<th scope="row"><label for="pli_gen_offset">Offset into PDF for team name</label></th>'."\n".
+        '<td><input type="text" id="pli_gen_offset" name="pli_gen_offset" value="'.($options['offset']).'"/></td>'."\n".
+        '</tr>'."\n".
+        '<tr valign="top">'."\n".
+        '<th scope="row"><label for="pli_gen_fontsize">Font size for team name</label></th>'."\n".
+        '<td><input type="text" id="pli_gen_fontsize" name="pli_gen_fontsize" value="'.($options['fontsize']).'"/></td>'."\n".
+        '</tr>'."\n".
+        '</table>'."\n".
+        '<p class="submit"><input type="submit" name="Submit" class="button-primary" value="Save changes"/></p>'."\n".
+        '<input type="hidden" name="pli_gen_options_nonce" value="'.wp_create_nonce( basename( __FILE__ ) ).'" />'."\n".
+        '</form>'."\n".
+        '</div>'."\n";
 }
 
 function pli_uninstall() {
